70aac1a59fa95989edb8817979265378b2be7ac9
pratham <pratcode660@gmail.com>
2021-07-15 18:26:14 +0530

    made newhome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import NewHome from "./components/newhome/NewHome";
 import Navbar from "./components/navbar";
 import HomePage from "./components/homepage/homepage";
 import AboutUs from "./components/aboutus/about";
@@ -10,6 +11,7 @@ import Footer from "./components/footer/footer";
 import Register from "./components/login2/logReg";
 import Approval from "./components/rms_approval/rms_approval";
 import Head_Approval from "./components/talent_head_approval/talent_head_approval";
+
 import {
   BrowserRouter as Router,
   Route,
@@ -20,23 +22,25 @@ import Sidebar from "./components/side_bar";
 function App() {
   return (
     <>
+    <Navbar/>
       <Router>
         <div className = "row tsb">
           
           <div className = "col lsb">
-          <Sidebar />
+          {/* <Sidebar /> */}
         </div>
         <div className= "col rsb">
           <Switch>
           <Route path="/home" exact component={HomePage} />
-          <Route path="/aboutus" exact component={AboutUs} />
-          <Route path="/careers" exact component={AboutUs} />
+          <Route path="/newhome" exact component={NewHome} />
+          {/* <Route path="/aboutus" exact component={AboutUs} /> */}
+          {/* <Route path="/careers" exact component={AboutUs} /> */}
           <Route path="/ATS" exact component={ReqList} />
           <Route path="/addreq" exact component={Form} />
           <Route path="/register" exact component={Register} />
           <Route path="/login" exact component={LoginForm2} />
-          <Route path="/projects" exact component={AboutUs} />
-          <Route path="/login" exact component={AboutUs} />
+          {/* <Route path="/projects" exact component={AboutUs} /> */}
+          {/* <Route path="/login" exact component={AboutUs} /> */}
           <Route path="/approval" exact component={Approval} />
           <Route path="/head_approval" exact component={Head_Approval} />
           <Redirect path="/" exact to="/home" />
@@ -46,7 +50,7 @@ function App() {
         </div>
         
       </Router>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Sidebar from "./side_bar";
-
+import notifimg from "../images/navbarimg/notif.jpeg";
+import globeimg from "../images/navbarimg/globe.jpeg";
+import avatarimg from "../images/navbarimg/avatar.jpeg";
 
 import "./navbar.css";
 import { react } from "@babel/types";
 
-const Navbar= () => {
-  return(
+const Navbar = () => {
+  return (
     <>
-    {/* <input type="checkbox" id="check"/>
-    <label class="button bars" for="check"><i class="fas fa-bars"></i></label> */}
-    <Sidebar/>
+      {/* <Sidebar/> */}
+      <div className="row" style={{textAlign:"right" ,  marginRight:"10px"}}>
+          <input type="text" name="search" placeholder="Search.." />
+          <button type="button" className="btn btn-primary">
+            {" "}
+            + Add New
+          </button>
+          <a href="#" className="globe">
+            <img src={globeimg} className="globeimg" alt="nn" />
+          </a>
+          <a href="#" className="notification" >
+            <img src={notifimg} alt="nn"  className="notifimg"/>
+            {/* <span className="badge">2</span> */}
+          </a>
+          <img src={avatarimg} alt="Avatar" className="avatar"/>
+        </div>
     </>
-  )
-}
+  );
+};
 export default Navbar;
